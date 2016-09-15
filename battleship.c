bd28b9349ed30bbb475657d8ba5cdd135f5e056b
raquel opazo <raquel opazo>
2016-09-15 14:37:08 -0400

    hola

--- battleship.c
+++ battleship.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main(){
+	char** arreglostring=malloc(sizeof(char*)*3);
+	arreglostring[2]="hola";
+	arreglostring[1]="chao";
+	arreglostring[0]="como estas?";
+	int i;
+	for (i=0;i<3;i++){
+			printf("%s\n",arreglostring[i] );
+
+
+	}
 
 	return 0;
 }
